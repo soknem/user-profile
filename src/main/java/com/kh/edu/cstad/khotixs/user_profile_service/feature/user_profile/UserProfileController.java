ee08d12ad76e2e8a9d9bb62c912348c9b8b55f68
soknem <sokname56@gmail.com>
2024-12-26 11:34:21 +0700

    complete get me

--- src/main/java/com/kh/edu/cstad/khotixs/user_profile_service/feature/user_profile/UserProfileController.java
+++ src/main/java/com/kh/edu/cstad/khotixs/user_profile_service/feature/user_profile/UserProfileController.java
@@ -4,6 +4,7 @@ import com.kh.edu.cstad.khotixs.user_profile_service.feature.user_profile.dto.Us
 import com.kh.edu.cstad.khotixs.user_profile_service.feature.user_profile.dto.UserProfileUpdateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user-profiles")
+@Slf4j
 public class UserProfileController {
     private final UserProfileService userProfileService;
 
     @GetMapping("/me")
-    public UserProfileResponse getMe(@AuthenticationPrincipal Authentication authentication){
+    public UserProfileResponse getMe(Authentication authentication){
 
+        log.info("Authentication:{}",authentication);
         return userProfileService.findMe(authentication);
     }
 
